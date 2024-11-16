e6a9d4d1f73707e8c1ea52cabbbec778d674e623
NayraSousa <nayraamaral1@gmail.com>
2024-11-16 16:01:19 -0300

    feat: add remove_pairs function

---
+++ recursion/remove_pairs.py
@@ -0,0 +1,10 @@
+def RemovePairs(number):
+    if number==0: return 0
+    else:
+        last_digit = number%10
+        if last_digit%2==0:
+            return RemovePairs(number//10)
+        return RemovePairs(number//10)*10+last_digit
+    
+number = int(input("Digite um número inteiro: "))
+print(RemovePairs(number))
