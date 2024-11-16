fe19341ed40f81e8328d8e7e461b28b3919cd238
NayraSousa <nayraamaral1@gmail.com>
2024-11-16 15:49:39 -0300

    feat: add zero_odd_numbers function

---
+++ recursion/zero_odd_numbers.py
@@ -0,0 +1,10 @@
+def ResetOdd(number):
+    if number==0: return 0
+    else:
+        last_digit = number%10
+        if last_digit%2!=0:
+            last_digit = 0
+        return ResetOdd(number//10)*10+last_digit
+    
+number = int(input("Digite um número inteiro: "))
+print(ResetOdd(number))
