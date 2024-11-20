0fc95693ca580504b80e19cde492d93a3661db26
Nayra de Sousa <nayraamaral1@gmail.com>
2024-11-20 15:35:57 -0300

    feat: add insertAtEnd function

--- doubly-linked-list/doubly_linked_list.py
+++ doubly-linked-list/doubly_linked_list.py
@@ -5,14 +5,52 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+        
     def insertAtBeginning(self, data):
         newNode = Node(data)
         if self.head==None:
             self.head = self.tail = newNode
+            self.length += 1
         else:
             newNode.previous = None
             newNode.next = self.head
             self.head.previous = newNode
             self.head = newNode
             self.length += 1
-        
+
+    def insertAtEnd(self, data):
+        if self.head==None:
+            self.head = Node(data)
+            self.length += 1
+        else:
+            currentNode = self.head
+            while currentNode.next != None:
+                currentNode = currentNode.next
+            newNode = Node(data)
+            currentNode.next = newNode
+            newNode.previous = currentNode
+            newNode.next = None
+            self.length += 1
+    
+    def print(self):
+        h = self.head
+        while(h):
+            print(f'{h.data} <->', end=' ')
+            h=h.next
+        print('\n')
+
+list = DoublyLinkedList()
+list.insertAtBeginning(4)
+list.insertAtBeginning(5)
+list.insertAtBeginning(6)
+list.insertAtBeginning(8)
+list.print()
+list.insertAtEnd(1)
+list.insertAtEnd(1)
+list.print()
+list.insertAtBeginning(8)
+list.print()
+print(list.length)
+
+
+
